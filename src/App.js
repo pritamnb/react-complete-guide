e3dd87ff54bf4cc40430c72082f3e6ae04e0a511
Pritam Bhalerao <pritam.bhalerao@codevian.com>
2020-07-06 00:45:10 +0530

    class based approach

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 // @ts-nocheck
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-const App = () => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { id: 1, name: 'Pritam', age: 24 },
       { id: 2, name: 'Manu', age: 29 },
@@ -11,81 +11,82 @@ const App = () => {
     ],
     otherState: 'Some other state',
     showPersons: true,
-  });
+  };
   // console.log(personsState);
 
-  const nameChangedHandler = (event, id) => {
+  nameChangedHandler = (event, id) => {
     console.log('*******************', event.target.value, id);
 
-    const personIndex = personsState.persons.findIndex((p) => {
+    const personIndex = this.state.persons.findIndex((p) => {
       return p.id === id;
     });
-    const person = { ...personsState.persons[personIndex] };
+    const person = { ...this.state.persons[personIndex] };
 
     // or
-    // const person = Object.assign({},personsState.persons[personIndex]);
+    // const person = Object.assign({},this.state.persons[personIndex]);
     person.name = event.target.value;
-    const changedPerson = [...personsState.persons];
+    const changedPerson = [...this.state.persons];
     console.log('############', person);
 
     changedPerson[personIndex] = person;
     console.log('*******************', changedPerson);
-    setPersonsState({
+    this.setState({
       persons: changedPerson,
-      ...personsState,
     });
-    console.log(personsState);
+    console.log(this.state);
   };
-  const deletePersonHandle = (peronIndex) => {
-    const persons = personsState.persons;
+  deletePersonHandle = (peronIndex) => {
+    const persons = this.state.persons;
     persons.splice(peronIndex, 1);
-    setPersonsState({ persons: persons, ...personsState });
+    this.setState({ persons: persons });
   };
-  const togglePersonHandler = () => {
-    const doesShow = personsState.showPersons;
-    setPersonsState({ ...personsState, showPersons: !doesShow });
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
   };
-  const style = {
-    backgroundColor: 'white',
-    font: 'inherit',
-    border: '1px solid blue',
-    padding: '8px',
-    cursor: 'pointer',
-  };
-  let persons = null;
-  if (personsState.showPersons) {
-    persons = (
-      <div>
-        {personsState.persons.map((person, index) => {
-          return (
-            <Person
-              click={() => deletePersonHandle(index)}
-              changed={(event) => nameChangedHandler(event, person.id)}
-              name={person.name}
-              age={person.age}
-              key={person.id}
-            />
-          );
-        })}
+  render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+    };
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandle(index)}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+              />
+            );
+          })}
+        </div>
+      );
+    }
+    return (
+      <div className='App'>
+        <h1> Hi, I'm a React app</h1>
+        <p>This is really working</p>
+        <button style={style} onClick={this.togglePersonHandler}>
+          {' '}
+          Toggle Name
+        </button>
+        {persons}
       </div>
     );
+    // return React.createElement(
+    //   'div',
+    //   { className: 'App' },
+    //   React.createElement('h1', null, "Hi, I'm a React App!!!")
+    // );
   }
-  return (
-    <div className='App'>
-      <h1> Hi, I'm a React app</h1>
-      <p>This is really working</p>
-      <button style={style} onClick={togglePersonHandler}>
-        {' '}
-        Toggle Name
-      </button>
-      {persons}
-    </div>
-  );
-  // return React.createElement(
-  //   'div',
-  //   { className: 'App' },
-  //   React.createElement('h1', null, "Hi, I'm a React App!!!")
-  // );
-};
+}
 
 export default App;

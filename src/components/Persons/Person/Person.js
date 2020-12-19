98cb0f3e3bf40ba1e45a02b91f64d2d5ef9a2868
pritamnb <preetambhalerao00@gmail.com>
2020-12-20 00:06:37 +0530

    created a higher order component called auxtag which return props children

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
 import classes from '../Person/Person.css';
+import Aux from '../../../hoc/AuxTag';
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
-    return [
-      <div key='r1' className={classes.Person}>
-        <p key='i1' onClick={this.props.click}>
-          I'm {this.props.name} and I'm {this.props.age} year(s) old!
-        </p>
-        <p key='i2'>{this.props.children}</p>
-        <input
-          key='i3'
-          type='text'
-          onChange={this.props.changed}
-          value={this.props.name}
-        ></input>
-      </div>,
-    ];
+    return (
+      <Aux>
+        <div key='r1' className={classes.Person}>
+          <p key='i1' onClick={this.props.click}>
+            I'm {this.props.name} and I'm {this.props.age} year(s) old!
+          </p>
+          <p key='i2'>{this.props.children}</p>
+          <input
+            key='i3'
+            type='text'
+            onChange={this.props.changed}
+            value={this.props.name}
+          ></input>
+        </div>
+      </Aux>
+    );
   }
 }
 
